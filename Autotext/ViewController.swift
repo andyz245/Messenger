7168d27a6522632b466fea567bd34cf39be60d82
Andy Zhou <andyzhou1989@gmail.com>
2019-06-19 21:01:18 -0700

    Initial Commit

---
+++ Autotext/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autotext
+//
+//  Created by Andy  Zhou on 6/19/19.
+//  Copyright © 2019 Andy  Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
