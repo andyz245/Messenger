5c3124da080007fdf775d54ed262581040d1ab5a
andyz245 <andyzhou1989@gmail.com>
2021-11-10 21:34:52 -0600

    imported code from  local

--- Autotext/ViewController.swift
+++ Autotext/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var messageOneButton: UIButton!
+    @IBOutlet weak var messageTwoButton: UIButton!
+    @IBOutlet weak var messageThreeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func useFirstMessage(_ sender: UIButton) {
+        self.sendMessage(messageOneButton)
+    }
+    
+    @IBAction func useSecondMessage(_ sender: UIButton) {
+        self.sendMessage(messageTwoButton)
+    }
+    
+    @IBAction func useThirdMessage(_ sender: UIButton) {
+        self.sendMessage(messageThreeButton)
+    }
+    
+    @IBAction func sendMessage(_ sender: UIButton) {
+        if MFMessageComposeViewController.canSendText() {
+            let messageController = MFMessageComposeViewController()
+            messageController.messageComposeDelegate = self
+            messageController.recipients = ["4087864220"]
+            messageController.body = sender.currentTitle
+            present(messageController, animated: false, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+        messageTextField.text = ""
+    }
+    
 }
 
